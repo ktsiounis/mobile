68a32610e236ab8801820f9e083710e2b00183fe
koukouzasg <koukouzasg@hotmail.gr>
2016-12-10 22:08:23 +0200

    setup almost done,main loop left

--- Simulation/main.m
+++ Simulation/main.m
@@ -1,13 +1,15 @@
 % Simulation details and contact info
 ...
+clear;
+clc;
 
 % Setup variables and constants
 setup();
 
 % Loop
-for <:>
+for i = 1:MAX_ITER
 ...
+    % Visualization
+    visualise();
 end
 
-% Visualization
-visualise();
--- Simulation/setup.m
+++ Simulation/setup.m
@@ -3,14 +3,23 @@
 %Mobile robot variables and constants
 m = 1;				% Mass of the mobile robot (kg)
 g = 9.17			% Gravity constant
+rad = 0.5;          % size of the wheel
+d = 2;              % distance between the wheels
+phi_dot_1 = 5;      %force of the motors needed for velocity.Will be calculated later
+phi_dot_2 = 5;      %force of the motors needed for velocity.Will be calculated later
+
 
 % Simulation variables and contstants
-starttime = 0;			% Times starts at 0 seconds
-endtime = 20;			% Time will end at 20 seconds
-dt = 0.001;			% Time derivative
+%starttime = 0;			% Times starts at 0 seconds
+MAX_ITER = 50;			% Time will end at 20 seconds,max iterations          
+DT = 0.05;              % Time derivative
+		
 
 % *_out vectors initialized for saving all the simulation
 % data for the visualization.
-x_out = zeros(2, N);		% Position vector
-v_out = zeros(2, N);		% Vecolity vector
-a_out = zeros(2, N);		% Acceleration vector
+x_out = zeros(2, 1);		% Position vector
+v_out = zeros(2, 1);		% Vecolity vector
+a_out = zeros(2, 1);		% Acceleration vector
+omega = zeros(2,1);         % Angular velocity of the drone,theta_dot=omega
+theta = zeros(2,1);         % Starting direction
+
