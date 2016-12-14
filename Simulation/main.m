c1ee7c238df576de9a05ee5461b12cf9cc4326a2
koukouzasg <koukouzasg@hotmail.gr>
2016-12-14 15:49:01 +0200

    kinematic added

--- Simulation/main.m
+++ Simulation/main.m
@@ -7,9 +7,21 @@ clc;
 setup();
 
 % Loop
-for i = 1:MAX_ITER
-...
-    % Visualization
-    visualise();
+for i = 1:N
+v(1) = (r*(sum(F_dot)/2))*cos(theta(1));
+v(2) = (r*(sum(F_dot)/2))*sin(theta(2));
+omega = r*(F_dot(2)-F_dot(1)/d);
+x = x + v*dt;
+theta = theta + omega*dt;
+a = a + v/dt;
+
+
+a_out(:,i) = a;
+v_out(:,i) = v;
+x_out(:,i) = x;
+omega_out(:,i) = omega;
+theta(:,i) = theta;
 end
 
+% Visualization
+visualise();
--- Simulation/setup.m
+++ Simulation/setup.m
@@ -5,8 +5,7 @@ m = 1;				% Mass of the mobile robot (kg)
 g = 9.17			% Gravity constant
 rad = 0.5;          % size of the wheel
 d = 2;              % distance between the wheels
-phi_dot_1 = 5;      %force of the motors needed for velocity.Will be calculated later
-phi_dot_2 = 5;      %force of the motors needed for velocity.Will be calculated later
+F_dot = [5; 5];		%force of the motors needed for velocity.Will be calculated later
 
 
 % Simulation variables and contstants
@@ -20,9 +19,15 @@ N = length(times);
 
 % *_out vectors initialized for saving all the simulation
 % data for the visualization.
-x_out = zeros(2, 1);		% Position vector
-v_out = zeros(2, 1);		% Vecolity vector
-a_out = zeros(2, 1);		% Acceleration vector
-omega = zeros(2,1);         % Angular velocity of the drone,theta_dot=omega
-theta = zeros(2,1);         % Starting direction
+x_out = zeros(2, N);		% Position vector
+v_out = zeros(2, N);		% Vecolity vector
+a_out = zeros(2, N);		% Acceleration vector
+omega_out = zeros(1,N);      % Angular velocity of the drone,theta_dot=omega
+theta_out = zeros(1,N);      % Starting direction
 
+%vector for calculating
+v = [0; 0];					
+a = [0; 0];
+x = [0; 0];
+omega = [0];
+theta = [0];
