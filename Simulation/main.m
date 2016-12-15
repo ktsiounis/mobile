e2563656a2e99d19917eaa9b226354cc84d75d71
koukouzasg <koukouzasg@hotmail.gr>
2016-12-15 03:44:25 +0200

    bugs

--- Simulation/main.m
+++ Simulation/main.m
@@ -6,11 +6,12 @@ clc;
 % Setup variables and constants
 setup();
 
+figure 
 % Loop
 for i = 1:N
-v(1) = (rad*(sum(F_dot)/2))*cos(theta(1));
-v(2) = (rad*(sum(F_dot)/2))*sin(theta(2));
-omega = r*(F_dot(2)-F_dot(1)/d);
+v(1) = ((rad*(sum(F_dot)/2))*cos(theta));   %X axis
+v(2) = (rad*(sum(F_dot)/2))*sin(theta);   %Y axis
+omega = rad*(F_dot(2)-F_dot(1)/d);
 x = x + v*dt;
 theta = theta + omega*dt;
 a = a + v/dt;
@@ -20,8 +21,12 @@ a_out(:,i) = a;
 v_out(:,i) = v;
 x_out(:,i) = x;
 omega_out(:,i) = omega;
-theta(:,i) = theta;
-end
+theta_out(:,i) = theta;
+
+scatter(x_out(1),x_out(2))
+hold on
+end;
 
 % Visualization
-visualise();
+%visualise();
+
